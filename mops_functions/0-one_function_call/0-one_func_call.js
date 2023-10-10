2765ea50ea563113220a9fca42c899671b95abf7
toonMarty <martinnyaga7373@gmail.com>
2023-10-10 16:48:35 +0300

    Remove test code

--- mops_functions/0-one_function_call/0-one_func_call.js
+++ mops_functions/0-one_function_call/0-one_func_call.js
@@ -10,10 +10,3 @@ var once = function(fn) {
         return counter === 1 ? fn(...args) : undefined;
     }
 };
-
-let fn = (a,b,c) => (a * b * c);
-
-const onceFn = once(fn);
-console.log(onceFn(5, 7, 4)); // 140
-console.log(onceFn(2, 3, 6)); // undefined, fn was not called
-console.log(onceFn(4, 6, 8)); // undefined, fn was not called
